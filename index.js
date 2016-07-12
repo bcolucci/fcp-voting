dc22349bff10f4bd241d7351cbef94872269fe6d
Brice Colucci <bcolucci@wuha.io>
2016-07-12 17:04:32 +0200

    wip

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const User = require('./lib/User');
+const Vote = require('./lib/Vote');
+const Option = require('./lib/Option');
+const Poll = require('./lib/Poll');
+
+module.exports = {
+  User, Vote, Option, Poll
+};
