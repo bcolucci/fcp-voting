bb9e36adfc74fc734f45c1b4c66fdbd3dffcda00
Brice Colucci <bcolucci@wuha.io>
2016-07-12 16:02:18 +0200

    wip

--- src/poll.js
+++ src/poll.js
@@ -1,11 +1,14 @@
-// @flow
+
+import I from 'immutable';
 
 export default class Poll {
 
   name: string;
+  options: I.Set<string>;
 
-  constructor(name: string) {
+  constructor(name: string, options?: I.Set<string> = I.Set()) {
     this.name = name;
+    this.options = options;
   }
 
 };
--- test/poll.js
+++ test/poll.js
@@ -1,11 +1,21 @@
 
+import I from 'immutable';
 import sinon from 'sinon';
 import Poll from '../src/poll';
 
-console.log(''.should);
+//TODO error cases
 
-describe('Voting application', () => {
+describe('Voting Pool', () => {
 
-  //TODO
+  it('should have a name', () => {
+    (new Poll('my poll')).should.have.property('name').and.be.String();
+  });
+
+  it('may have options', () => {
+    let poll = new Poll('my poll');
+    poll.should.have.property('options').and.be.deepEqual(I.Set());
+    poll.options = poll.options.add('Star Wars');
+    I.is(poll.options, I.Set([ 'Star Wars' ])).should.be.ok();
+  });
 
 });
